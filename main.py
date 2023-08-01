987bd60d9f172cdd722487dc2a66d8f2f8542443
Okan Hollander <okan.hollander@quality.nl>
2023-08-01 13:38:50 +0200

    docstring onzin

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # Cisco ASA restful API to obtain all address objects with the requests library
-import cisco
 from rich import print as rprint
+import cisco
 
 FILE_PATH = "credentials.ini"
-cisco_device = "ASA"
+CISCO_DEVICE = "ASA"
 
 # Accessing ASA
-cisco_device = cisco.Cisco(cisco_device, FILE_PATH)
+CISCO_DEVICE = cisco.Cisco(CISCO_DEVICE, FILE_PATH)
 
 if __name__ == "__main__":
-    static_routes = cisco_device.get_static_routes()
-    address_objects = cisco_device.get_network_objects()
+    static_routes = CISCO_DEVICE.get_static_routes()
+    address_objects = CISCO_DEVICE.get_network_objects()
     rprint(address_objects)
